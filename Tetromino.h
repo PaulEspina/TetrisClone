d98f40d95d2444c3d7a879924f017de33dccb81e
Paul Kristopher Espina <kristopherespina@gmail.com>
2021-08-26 21:01:57 +0800

    fixed: reinitializing Block when moving

--- Tetromino.h
+++ Tetromino.h
@@ -15,42 +15,13 @@ public:
         type = 0;
     }
 
-    Tetromino(sf::Vector2u gridPos, unsigned int type)
+    Tetromino(sf::Vector2u gridPos, unsigned int type, unsigned int rotation)
     {
         this->gridPos = gridPos;
         numOfBlocks = 4;
         blocks = new Block[numOfBlocks];
-        setType(type);
-        update();
-    }
-
-    ~Tetromino()
-    {
-        delete blocks;
-    }
-
-    void moveDown()
-    {
-        gridPos += sf::Vector2u(0, 1);
-    }
-
-    void moveUp()
-    {
-        gridPos += sf::Vector2u(0, -1);
-    }
-
-    void moveLeft()
-    {
-        gridPos += sf::Vector2u(-1, 0);
-    }
-
-    void moveRight()
-    {
-        gridPos += sf::Vector2u(1, 0);
-    }
-
-    void update()
-    {
+        this->type = type;
+        this->rotation = rotation;
         switch(type)
         {
         case 0:
@@ -96,11 +67,102 @@ public:
             blocks[3] = Block(gridPos + sf::Vector2u(2, 1), type);
             break;
         }
+
+        update();
+    }
+
+    ~Tetromino()
+    {
+        delete blocks;
+    }
+
+    void turnClockwise()
+    {
+    }
+
+    void turnCounterClockwise()
+    {
+
+    }
+
+    void moveDown()
+    {
+        gridPos += sf::Vector2u(0, 1);
+    }
+
+    void moveUp()
+    {
+        gridPos += sf::Vector2u(0, -1);
+    }
+
+    void moveLeft()
+    {
+        gridPos += sf::Vector2u(-1, 0);
+    }
+
+    void moveRight()
+    {
+        gridPos += sf::Vector2u(1, 0);
+    }
+
+    void update()
+    {
+        switch(type)
+        {
+        case 0:
+            blocks[0].setGridPos(gridPos);
+            blocks[1].setGridPos(gridPos + sf::Vector2u(1, 0));
+            blocks[2].setGridPos(gridPos + sf::Vector2u(2, 0));
+            blocks[3].setGridPos(gridPos + sf::Vector2u(3, 0));
+            break;
+        case 1:
+
+            blocks[0].setGridPos(gridPos);
+            blocks[1].setGridPos(gridPos + sf::Vector2u(1, 0));
+            blocks[2].setGridPos(gridPos + sf::Vector2u(0, 1));
+            blocks[3].setGridPos(gridPos + sf::Vector2u(1, 1));
+            break;
+        case 2:
+            blocks[0].setGridPos(gridPos);
+            blocks[1].setGridPos(gridPos + sf::Vector2u(1, 0));
+            blocks[2].setGridPos(gridPos + sf::Vector2u(2, 0));
+            blocks[3].setGridPos(gridPos + sf::Vector2u(1, 1));
+            break;
+        case 3:
+            blocks[0].setGridPos(gridPos + sf::Vector2u(1, 0));
+            blocks[1].setGridPos(gridPos + sf::Vector2u(1, 1));
+            blocks[2].setGridPos(gridPos + sf::Vector2u(1, 2));
+            blocks[3].setGridPos(gridPos + sf::Vector2u(0, 2));
+            break;
+        case 4:
+            blocks[0].setGridPos(gridPos);
+            blocks[1].setGridPos(gridPos + sf::Vector2u(0, 1));
+            blocks[2].setGridPos(gridPos + sf::Vector2u(0, 2));
+            blocks[3].setGridPos(gridPos + sf::Vector2u(1, 2));
+            break;
+        case 5:
+            blocks[0].setGridPos(gridPos + sf::Vector2u(1, 0));
+            blocks[1].setGridPos(gridPos + sf::Vector2u(2, 0));
+            blocks[2].setGridPos(gridPos + sf::Vector2u(1, 1));
+            blocks[3].setGridPos(gridPos + sf::Vector2u(0, 1));
+            break;
+        case 6:
+            blocks[0].setGridPos(gridPos);
+            blocks[1].setGridPos(gridPos + sf::Vector2u(1, 0));
+            blocks[2].setGridPos(gridPos + sf::Vector2u(1, 1));
+            blocks[3].setGridPos(gridPos + sf::Vector2u(2, 1));
+            break;
+        }
     }
 
     void setType(unsigned int type)
     {
         this->type = type;
+    }
+
+    void setRotation(unsigned int rotation)
+    {
+        this->rotation = rotation;
     }
 
     Block* getBlocks()
@@ -115,6 +177,7 @@ public:
 
 private:
     unsigned int type;
+    unsigned int rotation;
     Block* blocks;
     unsigned int numOfBlocks;
     sf::Vector2u gridPos;

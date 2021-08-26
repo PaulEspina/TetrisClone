a3eb166aaa09ff2c2a28debc82edb594d2207634
Paul Kristopher Espina <kristopherespina@gmail.com>
2021-08-26 18:29:40 +0800

    added constructor that creates different types of tetrominos

--- Tetromino.h
+++ Tetromino.h
@@ -14,6 +14,73 @@ public:
         this->numOfBlocks = numOfBlocks;
     }
 
+    Tetromino(sf::Vector2u gridPos, unsigned int type)
+    {
+        this->gridPos = gridPos;
+        switch(type)
+        {
+        case 0:
+            numOfBlocks = 4;
+            blocks = new Block[numOfBlocks];
+            blocks[0] = Block(gridPos, type);
+            blocks[1] = Block(gridPos + sf::Vector2u(1, 0), type);
+            blocks[2] = Block(gridPos + sf::Vector2u(2, 0), type);
+            blocks[3] = Block(gridPos + sf::Vector2u(3, 0), type);
+            break;
+        case 1:
+            numOfBlocks = 4;
+            blocks = new Block[numOfBlocks];
+            blocks[0] = Block(gridPos, type);
+            blocks[1] = Block(gridPos + sf::Vector2u(1, 0), type);
+            blocks[2] = Block(gridPos + sf::Vector2u(0, 1), type);
+            blocks[3] = Block(gridPos + sf::Vector2u(1, 1), type);
+            break;
+        case 2:
+            numOfBlocks = 4;
+            blocks = new Block[numOfBlocks];
+            blocks[0] = Block(gridPos, type);
+            blocks[1] = Block(gridPos + sf::Vector2u(1, 0), type);
+            blocks[2] = Block(gridPos + sf::Vector2u(2, 0), type);
+            blocks[3] = Block(gridPos + sf::Vector2u(1, 1), type);
+            break;
+        case 3:
+            numOfBlocks = 4;
+            blocks = new Block[numOfBlocks];
+            blocks[0] = Block(gridPos + sf::Vector2u(1, 0), type);
+            blocks[1] = Block(gridPos + sf::Vector2u(1, 1), type);
+            blocks[2] = Block(gridPos + sf::Vector2u(1, 2), type);
+            blocks[3] = Block(gridPos + sf::Vector2u(0, 2), type);
+            break;
+        case 4:
+            numOfBlocks = 4;
+            blocks = new Block[numOfBlocks];
+            blocks[0] = Block(gridPos, type);
+            blocks[1] = Block(gridPos + sf::Vector2u(0, 1), type);
+            blocks[2] = Block(gridPos + sf::Vector2u(0, 2), type);
+            blocks[3] = Block(gridPos + sf::Vector2u(1, 2), type);
+            break;
+        case 5:
+            numOfBlocks = 4;
+            blocks = new Block[numOfBlocks];
+            blocks[0] = Block(gridPos + sf::Vector2u(1, 0), type);
+            blocks[1] = Block(gridPos + sf::Vector2u(2, 0), type);
+            blocks[2] = Block(gridPos + sf::Vector2u(1, 1), type);
+            blocks[3] = Block(gridPos + sf::Vector2u(0, 1), type);
+            break;
+        case 6:
+            numOfBlocks = 4;
+            blocks = new Block[numOfBlocks];
+            blocks[0] = Block(gridPos, type);
+            blocks[1] = Block(gridPos + sf::Vector2u(1, 0), type);
+            blocks[2] = Block(gridPos + sf::Vector2u(1, 1), type);
+            blocks[3] = Block(gridPos + sf::Vector2u(2, 1), type);
+            break;
+        default:
+            numOfBlocks = 0;
+            blocks = new Block[0];
+        }
+    }
+
     void fall()
     {
         for(unsigned int i = 0; i < numOfBlocks; i++)
@@ -44,7 +111,13 @@ public:
         return blocks;
     }
 
+    unsigned int getNumOfBlocks()
+    {
+        return numOfBlocks;
+    }
+
 private:
     Block* blocks;
     unsigned int numOfBlocks;
+    sf::Vector2u gridPos;
 };

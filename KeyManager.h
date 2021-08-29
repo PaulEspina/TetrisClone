c71d9198f413b23c99b99b82932eb908d6ac70bf
Paul Kristopher Espina <kristopherespina@gmail.com>
2021-08-29 22:41:40 +0800

    added KeyManager class

---
+++ KeyManager.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <SFML/Window.hpp>
+
+class KeyManager
+{
+public:
+	KeyManager()
+	{
+		keys = new int[sf::Keyboard::KeyCount];
+		resetKeys();
+	}
+
+	void update(sf::Event event)
+	{
+        if(event.type == sf::Event::KeyPressed)
+        {
+			keys[event.key.code] = true;
+        }
+		if(event.type == sf::Event::KeyReleased)
+		{
+			keys[event.key.code] = false;
+		}
+	}
+
+	/*void press(int key)
+	{
+		keys[key] = true;
+	}*/
+
+	bool isDown(int key)
+	{
+		return keys[key];
+	}
+
+	void resetKeys()
+	{
+		for(int i = 0; i < sf::Keyboard::KeyCount; i++)
+		{
+			keys[i] = false;
+		}
+	}
+private:
+	int *keys;
+};

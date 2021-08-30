dbd4b4144805e6238984fbb3520f097e35e217e4
Paul Kristopher Espina <kristopherespina@gmail.com>
2021-08-30 15:24:44 +0800

    added feature to detect key down, key released and key pressed separately

--- KeyManager.h
+++ KeyManager.h
@@ -7,39 +7,61 @@ class KeyManager
 public:
 	KeyManager()
 	{
-		keys = new int[sf::Keyboard::KeyCount];
+		keyDown = new int[sf::Keyboard::KeyCount];
+		keyReleased = new int[sf::Keyboard::KeyCount];
+		keyPressed = new int[sf::Keyboard::KeyCount];
 		reset();
+	}
+
+	void init()
+	{
+		for(int i = 0; i < sf::Keyboard::KeyCount; i++)
+		{
+			keyReleased[i] = false;
+			keyPressed[i] = false;
+		}
 	}
 
 	void update(sf::Event event)
 	{
         if(event.type == sf::Event::KeyPressed)
         {
-			keys[event.key.code] = true;
+			keyDown[event.key.code] = true;
+			keyPressed[event.key.code] = true;
         }
 		if(event.type == sf::Event::KeyReleased)
 		{
-			keys[event.key.code] = false;
+			keyDown[event.key.code] = false;
+			keyReleased[event.key.code] = true;
 		}
 	}
 
 	bool isDown(int key)
 	{
-		return keys[key];
+		return keyDown[key];
+	}
+
+	bool isPressed(int key)
+	{
+		return keyPressed[key];
+	}
+
+	bool isReleased(int key)
+	{
+		return keyReleased[key];
 	}
 
 	void reset()
 	{
 		for(int i = 0; i < sf::Keyboard::KeyCount; i++)
 		{
-			keys[i] = false;
+			keyDown[i] = false;
+			keyReleased[i] = false;
+			keyPressed[i] = false;
 		}
 	}
-
-	void reset(int key)
-	{
-		keys[key] = false;
-	}
 private:
-	int *keys;
+	int *keyDown;
+	int *keyPressed;
+	int *keyReleased;
 };

1eb9bdb5793d2deb1fd90b2eab823a7dfc1c8647
Paul Kristopher Espina <kristopherespina@gmail.com>
2021-09-02 17:10:01 +0800

    added MovementSettings class

---
+++ MovementSettings.cpp
@@ -0,0 +1,156 @@
+#include "MovementSettings.h"
+
+MovementSettings::MovementSettings()
+{
+	lockDelay = 0.0f;
+	lockDelayLeft = 0.0f;
+	lockTimerRunning = false;
+	fallDelay = 0.0f;
+	das = 0.0f;
+	moveSpeed = 0;
+	softDropSpeed = 0;
+}
+
+MovementSettings::MovementSettings(float lockDelay, float fallDelay, float das, unsigned int moveSpeed, unsigned int softDropSpeed)
+{
+	this->lockDelay = lockDelay;
+	lockDelayLeft = lockDelay;
+	lockTimerRunning = false;
+	this->fallDelay = fallDelay;
+	this->das = das;
+	this->moveSpeed = moveSpeed;
+	this->softDropSpeed = softDropSpeed;
+}
+
+void MovementSettings::init()
+{
+	lockTimerRunning = false;
+	lockTimer.restart();
+	fallTimer.restart();
+	dasTimer.restart();
+	moveTimer.restart();
+	softDropTimer.restart();
+}
+
+void MovementSettings::runLockTimer()
+{
+	lockTimerRunning = true;
+	lockTimer.restart();
+}
+
+void MovementSettings::stopLockTimer()
+{
+	lockTimerRunning = true;
+	lockDelayLeft -= lockTimer.getElapsedTime().asSeconds();
+	lockTimer.restart();
+}
+
+void MovementSettings::restartLockTimer()
+{
+	lockDelayLeft = lockDelay;
+}
+
+void MovementSettings::restartFallTimer()
+{
+	fallTimer.restart();
+}
+
+void MovementSettings::restartDASTimer()
+{
+	dasTimer.restart();
+}
+
+void MovementSettings::restartMoveTimer()
+{
+	moveTimer.restart();
+}
+
+void MovementSettings::restartSoftDropTimer()
+{
+	softDropTimer.restart();
+}
+
+bool MovementSettings::shouldLock()
+{
+	return lockDelayLeft < 0 ? true : false;
+}
+
+bool MovementSettings::shouldFall()
+{
+	return fallTimer.getElapsedTime().asSeconds() > fallDelay ? true : false;
+}
+
+bool MovementSettings::shouldDAS()
+{
+	return dasTimer.getElapsedTime().asSeconds() > das ? true : false;
+}
+
+bool MovementSettings::shouldMove()
+{
+	return moveTimer.getElapsedTime().asSeconds() > moveSpeed ? true : false;
+}
+
+bool MovementSettings::shouldSoftDrop()
+{
+	return softDropTimer.getElapsedTime().asSeconds() > softDropSpeed ? true : false;
+}
+
+float MovementSettings::getLockDelay()
+{
+	return lockDelay;
+}
+
+void MovementSettings::setLockDelay(float lockDelay)
+{
+	this->lockDelay = lockDelay;
+}
+
+bool MovementSettings::isLockTimerRunning()
+{
+	return lockTimerRunning;
+}
+
+void MovementSettings::setLockTimerRunning(bool value)
+{
+	lockTimerRunning = value;
+}
+
+float MovementSettings::getFallDelay()
+{
+	return fallDelay;
+}
+
+void MovementSettings::setFallDelay(float fallDelay)
+{
+	this->fallDelay = fallDelay;
+}
+
+float MovementSettings::getDAS()
+{
+	return das;
+}
+
+void MovementSettings::setDAS(float das)
+{
+	this->das = das;
+}
+
+unsigned int MovementSettings::getMoveSpeed()
+{
+	return moveSpeed;
+}
+
+void MovementSettings::setMoveSpeed(unsigned int moveSpeed)
+{
+	this->moveSpeed = moveSpeed;
+}
+
+unsigned int MovementSettings::getSoftDropSpeed()
+{
+	return softDropSpeed;
+}
+
+void MovementSettings::setSoftDropSpeed(unsigned int softDropSpeed)
+{
+	this->softDropSpeed;
+}
---
+++ MovementSettings.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <SFML/System.hpp>
+
+class MovementSettings
+{
+public:
+	MovementSettings();
+	MovementSettings(float lockDelay, float fallDelay, float das, unsigned int moveSpeed, unsigned int softDropSpeed);
+
+	void init();
+	void runLockTimer();
+	void stopLockTimer();
+	void restartLockTimer();
+	void restartFallTimer();
+	void restartDASTimer();
+	void restartMoveTimer();
+	void restartSoftDropTimer();
+	bool shouldLock();
+	bool shouldFall();
+	bool shouldDAS();
+	bool shouldMove();
+	bool shouldSoftDrop();
+
+	float getLockDelay();
+	void setLockDelay(float lockDelay);
+	bool isLockTimerRunning();
+	void setLockTimerRunning(bool value);
+	float getFallDelay();
+	void setFallDelay(float fallDelay);
+	float getDAS();
+	void setDAS(float das);
+	unsigned int getMoveSpeed();
+	void setMoveSpeed(unsigned int moveSpeed);
+	unsigned int getSoftDropSpeed();
+	void setSoftDropSpeed(unsigned int softDropSpeed);
+
+private:
+	float lockDelay;
+	float lockDelayLeft;
+	float fallDelay;
+	float das;
+	unsigned int moveSpeed;
+	unsigned int softDropSpeed;
+	bool lockTimerRunning;
+	sf::Clock lockTimer;
+	sf::Clock fallTimer;
+	sf::Clock dasTimer;
+	sf::Clock moveTimer;
+	sf::Clock softDropTimer;
+};
+
